34ac00257994126696de7f83415c17ad56829d57
AthitiJames <james@odds.team>
2024-09-12 17:53:40 +0700

    create mailer and create job due date checker

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,9 @@
 class UserMailer < ApplicationMailer
+  default from: 'notifications@example.com'
+
+  def due_date_email(user, todo)
+    @user = user
+    @todo = todo
+    mail(to: @user.email, subject: 'Your Todo has reached its due date!')
+  end
 end
