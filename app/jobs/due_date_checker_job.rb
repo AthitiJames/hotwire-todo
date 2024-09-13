5e93db5ef9af39cca79a7b74b30787efb0022b7f
AthitiJames <james@odds.team>
2024-09-13 10:07:23 +0700

    edit condition for check send mail

--- app/jobs/due_date_checker_job.rb
+++ app/jobs/due_date_checker_job.rb
@@ -1,12 +1,13 @@
 class DueDateCheckerJob < ApplicationJob
   queue_as :default
+
   def perform
-    # todos_due_today = Todo.where('due_date = ?', Date.current)
-    todos_due_today = Todo.where('due_date = ?', Date.current)
+    # ค้นหา todos ที่ due_date ภายในวันนี้
+    todos_due_today = Todo.where(due_date: Time.current.beginning_of_day..Time.current.end_of_day)
 
     todos_due_today.each do |todo|
-      user = todo.user
-      UserMailer.due_date_email(user, todo).deliver_now
+      user = todo.user  # ตอนนี้สามารถเรียก user จาก todo ได้แล้ว
+      UserMailer.due_date_email(user, todo).deliver_now  # ส่งอีเมลแจ้งเตือน
     end
   end
 end
