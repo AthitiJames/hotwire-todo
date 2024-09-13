d809e7d3e7a8966f3437a39bfaf1862646f55f37
AthitiJames <james@odds.team>
2024-09-13 10:12:25 +0700

    remove relation between user and todo and change to find user from user_id of todo

--- app/jobs/due_date_checker_job.rb
+++ app/jobs/due_date_checker_job.rb
@@ -6,7 +6,8 @@ class DueDateCheckerJob < ApplicationJob
     todos_due_today = Todo.where(due_date: Time.current.beginning_of_day..Time.current.end_of_day)
 
     todos_due_today.each do |todo|
-      user = todo.user  # ตอนนี้สามารถเรียก user จาก todo ได้แล้ว
+      user_id = todo.user_id
+      user = User.find(user_id)
       UserMailer.due_date_email(user, todo).deliver_now  # ส่งอีเมลแจ้งเตือน
     end
   end
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,6 +1,4 @@
 class Todo < ApplicationRecord
-    belongs_to :user
-
     validates_presence_of :name
     enum status: {
         incomplete: 0,
