2d1ad26926bc9b7773655987bc8c0c7351f77701
AthitiJames <james@odds.team>
2024-09-14 09:04:56 +0700

    use action cable with todos

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -4,4 +4,9 @@ class Todo < ApplicationRecord
         incomplete: 0,
         complete: 1
     }
+
+    after_create_commit { broadcast_prepend_later_to 'todos', target: 'todos-list' }
+    after_update_commit { broadcast_replace_later_to 'todos' }
+    after_destroy_commit { broadcast_remove_to 'todos' }
+
 end
